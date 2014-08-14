b624f32df108aeb20ab2c3709ad72ca7ff172c06
Ian Henderson <ianwhen@gmail.com>
2014-08-14 00:12:58 -0700

    Added fields to shipment model

--- api/models/Shipment.js
+++ api/models/Shipment.js
@@ -8,7 +8,54 @@
 module.exports = {
 
   attributes: {
-
+    name: {
+      type: 'string',
+      defaultsTo: null
+    },
+    destination_address: {
+      type: 'string',
+      defaultsTo: null
+    },
+    destination_latitude: {
+      type: 'float',
+      defaultsTo: null
+    },
+    destination_longitude: {
+      type: 'float',
+      defaultsTo: null
+    },
+    origin_address: {
+      type: 'string',
+      defaultsTo: null
+    },
+    origin_latitude: {
+      type: 'float',
+      defaultsTo: null
+    },
+    origin_longitude: {
+      type: 'float',
+      defaultsTo: null
+    },
+    package_height: {
+      type: 'float',
+      defaultsTo: null
+    },
+    package_width: {
+      type: 'float',
+      defaultsTo: null
+    },
+    package_depth: {
+      type: 'float',
+      defaultsTo: null
+    },
+    package_weight: {
+      type: 'float',
+      defaultsTo: null
+    },
+    cost: {
+      type: 'float',
+      defaultsTo: null
+    }
   }
 };
 
