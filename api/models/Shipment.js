cd8354613b8001c71b353fce42804605415bb0cf
Ian Henderson <ianwhen@gmail.com>
2014-08-14 02:55:23 -0700

    Added handler for Google Maps geocoding

--- api/models/Shipment.js
+++ api/models/Shipment.js
@@ -4,6 +4,8 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+var request = require('request');
+var googlemapsAPIKey = sails.config.googlemapsAPIKey;
 
 module.exports = {
 
@@ -56,6 +58,40 @@ module.exports = {
       type: 'float',
       defaultsTo: null
     }
+  },
+
+  // Google Maps API call for geocoding of address
+  beforeCreate: function(values, cb){
+
+    var baseURI = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+    var key = '&key=' + googlemapsAPIKey;
+
+    var re = new RegExp(' ', 'g');
+    var address = values.destination_address.replace(re, '+');
+    var url = baseURI + address + key;
+
+    request(url, function(err, res, body){
+
+      if (err) {
+        console.log(err);
+      }
+
+      var response = JSON.parse(body);
+
+      if (response.status === 'OK') {
+
+        var geometry = response.results[0].geometry;
+
+        if (geometry && geometry.location_type !== 'APPROXIMATE') {
+
+          values.destination_latitude = geometry.location.lat;
+          values.destination_longitude = geometry.location.lng;
+        }
+      }
+
+      cb();
+      
+    });
   }
 };
 
